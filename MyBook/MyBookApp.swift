6fa5afd804fcaa0f69e649422a874909964caee6
shubham lahoti <lahotishubham03@gmail.com>
2025-01-11 01:14:52 +0530

    Initial Commit

---
+++ MyBook/MyBookApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyBookApp.swift
+//  MyBook
+//
+//  Created by USER on 11/01/25.
+//
+
+import SwiftUI
+
+@main
+struct MyBookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
